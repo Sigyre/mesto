b4a2a1ba5313c72d1296d5172fe0d9ac08502e8d
Mariana Kabardina <maha@cleangames.ru>
2022-03-04 23:08:18 +0300

    Готовая работа v.1

--- index.js
+++ index.js
@@ -2,26 +2,32 @@ const popup = document.querySelector('.popup'); // ищем сам попап
 const openPopup = document.querySelector('.profile__edit-button'); //ищем кнопку редактирования профиля
 const closePopup = popup.querySelector('.popup__close'); //ищем крестик, кнопка закрытия редактирования профиля
 const savePopup = popup.querySelector('.popup__save'); // ищем кнопку сохранить 
-
+const autorName = document.querySelector('.profile__name'); // ищем имя в ПРОФИЛЕ
+const autorProfession = document.querySelector('.profile__profession'); // ищем профессию в ПРОФИЛЕ
+const popupAutorName = popup.querySelector('.popup__name'); // ищем имя в ПАПАПЕ
+const popupProfession = popup.querySelector('.popup__profession'); // ищем профессиию в ПАПАПЕ
 
 openPopup.addEventListener('click', function() { // слушаем клик по кнопке редактирования профиля и делаем папап видимым
   popup.classList.add('popup__open') 
+  popupAutorName.value = autorName.textContent; // передаем значение имени из уже заполненных данных
+  popupProfession.value = autorProfession.textContent;// передаем значение фамилии из уже заполненных данных
+
 });
 
 closePopup.addEventListener('click', function(){ // слушаем кнопку крестик, закрытия редактирования профиля, делаем папап невидимым
    popup.classList.remove('popup__open')
+   
+
 });
+
+
+
 
 
 const submitBtn = document.querySelector('.popup__form'); // ищем форму
 
 function formSubmitHandler (evt) { // пишем функцию замены
 	evt.preventDefault(); 
-	
-    const autorName = document.querySelector('.profile__name'); // ищем имя в ПРОФИЛЕ
-    const autorProfession = document.querySelector('.profile__profession'); // ищем профессию в ПРОФИЛЕ
-    const popupAutorName = popup.querySelector('.popup__name'); // ищем имя в ПАПАПЕ
-    const popupProfession = popup.querySelector('.popup__profession'); // ищем профессиию в ПАПАПЕ
 
     autorName.textContent = popupAutorName.value; // записываем новое имя
     autorProfession.textContent = popupProfession.value; // записываем новую профессию 
